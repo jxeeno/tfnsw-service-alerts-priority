877ec90d42c540d10beeece7886e8d7dbaeccd2c
Kenneth Tsang <me@jxeeno.com>
2022-06-27 02:09:59 +1000

    feat(): crash

--- src/index.js
+++ src/index.js
@@ -127,10 +127,12 @@ const getMatchedAlerts = async () => {
                 header.text = '🛠🛤 ' + header.text.trim()
             }else if(cause === 9 && effect === 6){ // 9 === 'MAINTENANCE', 6 === 'MODIFIED_SERVICE'
                 header.text = '🛠 ' + header.text.trim()
-            }else if(cause === 8){
+            }else if(cause === 8){ // weather
                 header.text = '🌨 ' + header.text.trim()
-            }else if(effect === 6 || effect === 4){
+            }else if(effect === 6 || effect === 4){ // modified_service or detour
                 header.text = '🔀 ' + header.text.trim()
+            }else if(cause === 6){ // accident
+                header.text = '💥 ' + header.text.trim()
             }
         }
 
